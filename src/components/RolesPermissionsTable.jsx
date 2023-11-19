eb0656f65e32cd87b1f54c8b43e0c9e33ae18de3
Tony Ale <78830055+bleach41@users.noreply.github.com>
2023-11-19 05:54:16 -0500

    modulado y perfecto hasta el mouseover de los roles

--- src/components/RolesPermissionsTable.jsx
+++ src/components/RolesPermissionsTable.jsx
@@ -1,20 +1,16 @@
-// Dentro del componente RolesPermissionsTable
+// RolesPermissionsTable.js
 import React, { useState, useEffect } from 'react';
+import RolesTable from './RolesTable';
 import AddPermissionModal from './AddPermissionModal';
 
 const RolesPermissionsTable = ({ roles, permissions }) => {
-
     const [newRole, setNewRole] = useState('');
     const [showAddPermissionModal, setShowAddPermissionModal] = useState(false);
     const [updatedPermissions, setUpdatedPermissions] = useState(JSON.parse(JSON.stringify(permissions)));
     const [updatedRoles, setUpdatedRoles] = useState(JSON.parse(JSON.stringify(roles)));
     const [entities, setEntities] = useState([...new Set(permissions.map(permission => permission.split(":")[0]))]);
-
-
-    // Nuevos estados para el checkbox de roles
     const [selectedRole, setSelectedRole] = useState(null);
     const [selectAllPermissions, setSelectAllPermissions] = useState({});
-
 
     useEffect(() => {
         // Inicializar selectAllPermissions cuando el componente se monta
@@ -60,8 +56,6 @@ const RolesPermissionsTable = ({ roles, permissions }) => {
         }
     };
 
-
-
     const handleAddRole = () => {
         // Validar que el nombre del nuevo rol no esté vacío
         if (newRole.trim() === '') {
@@ -89,8 +83,8 @@ const RolesPermissionsTable = ({ roles, permissions }) => {
         setNewRole('');
         console.log(`roles actuales${JSON.stringify(updatedRoles)}`)
         console.log(`ROLES ORIGINALES:   ${JSON.stringify(roles)}`);
-
     };
+
     const handleAddPermission = (newPermission) => {
 
         // Obtener la entidad y el permiso del nuevo permiso
@@ -113,119 +107,41 @@ const RolesPermissionsTable = ({ roles, permissions }) => {
         console.log(`PERMISOS ORIGINALES:   ${permissions}`)
     };
 
-    const renderTable = () => {
-
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        {entities.map(entity => (
-                            <React.Fragment key={entity}>
-                                <th colSpan={getEntityPermissions(entity).length}>{entity}</th>
-                            </React.Fragment>
-                        ))}
-                        <th colSpan="2">
-                            <button onClick={() => setShowAddPermissionModal(true)}>Añadir Nuevo Permiso</button>
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>Roles</th>
-                        {entities.flatMap(entity => (
-                            getEntityPermissions(entity).map(permission => (
-                                <React.Fragment key={`${entity}_${permission}`}>
-                                    <th key={`${entity}_${permission}`}>{permission}</th>
-                                </React.Fragment>
-                            ))
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {updatedRoles.map(role => (
-                        <React.Fragment key={role.id}>
-                            <tr onMouseOver={() => handleRoleMouseOver(role)} onMouseOut={handleRoleMouseOut}>
-                                <td>
-                                    {/* Checkbox para seleccionar/deseleccionar todos los permisos para el rol */}
-                                    {selectedRole === role && (
-                                        <input
-                                            type="checkbox"
-                                            checked={selectAllPermissions}
-                                            onChange={handleSelectAllPermissions}
-                                        />
-                                    )}
-                                    {role.name}
-                                </td>
-                                {entities.flatMap(entity => (
-                                    getEntityPermissions(entity).map(permission => (
-                                        <React.Fragment key={`${entity}_${permission}`}>
-                                            <td>
-                                                {role.permissions.includes(`${entity}:${permission}`) ? 'X' : ''}
-                                            </td>
-                                        </React.Fragment>
-                                    ))
-                                ))}
-                            </tr>
-                            {role.name === newRole && (
-                                <tr>
-                                    <td></td>
-                                    {entities.flatMap(entity => (
-                                        getEntityPermissions(entity).map(permission => (
-                                            <React.Fragment key={`${entity}_${permission}`}>
-                                                <td>
-                                                    {updatedPermissions.includes(`${entity}:${permission}`) ? 'X' : ''}
-                                                </td>
-                                            </React.Fragment>
-                                        ))
-                                    ))}
-                                    <td colSpan="2"></td>
-                                </tr>
-                            )}
-                        </React.Fragment>
-                    ))}
-                </tbody>
-
-                <tfoot>
-                    <tr>
-                        <td>
-                            {/* Campo para ingresar el nuevo rol */}
-                            <input
-                                type="text"
-                                value={newRole}
-                                onChange={(e) => setNewRole(e.target.value)}
-                                placeholder="Nuevo Rol"
-                            />
-                        </td>
-                        <td colSpan={entities.length * 2}>
-                            <button onClick={handleAddRole}>Añadir Nuevo Rol</button>
-                        </td>
-                    </tr>
-                </tfoot>
-            </table>
-        )
-    };
-
     const getEntityPermissions = (entity) => {
         return updatedPermissions
             .filter(permission => permission.startsWith(`${entity}:`))
             .map(permission => permission.split(":")[1]);
     };
 
-
-
     return (
         <div>
-            {renderTable()}
+            {/* Componente de la tabla de roles */}
+            <RolesTable
+                roles={roles}
+                updatedRoles={updatedRoles}
+                entities={entities}
+                getEntityPermissions={getEntityPermissions}
+                handleRoleMouseOver={handleRoleMouseOver}
+                handleRoleMouseOut={handleRoleMouseOut}
+                selectedRole={selectedRole}
+                selectAllPermissions={selectAllPermissions}
+                handleSelectAllPermissions={handleSelectAllPermissions}
+                newRole={newRole}
+                updatedPermissions={updatedPermissions}
+                handleAddRole={handleAddRole}
+                setShowAddPermissionModal={setShowAddPermissionModal}
+                setNewRole={setNewRole}
+            />
 
-            {showAddPermissionModal && (
-                <AddPermissionModal
-                    showModal={showAddPermissionModal}
-                    onClose={() => setShowAddPermissionModal(false)}
-                    onAddPermission={handleAddPermission}
-                    entities={entities}
-                />
-            )}
+            {/* Componente del modal de agregar permisos */}
+            <AddPermissionModal
+                showModal={showAddPermissionModal}
+                onClose={() => setShowAddPermissionModal(false)}
+                onAddPermission={handleAddPermission}
+                entities={entities}
+            />
         </div>
-    )
-}
+    );
+};
 
 export default RolesPermissionsTable;
---
+++ src/components/RolesTable.jsx
@@ -0,0 +1,110 @@
+// RolesTable.js
+import React from 'react';
+
+const RolesTable = ({
+    roles,
+    updatedRoles,
+    entities,
+    getEntityPermissions,
+    handleRoleMouseOver,
+    handleRoleMouseOut,
+    selectedRole,
+    selectAllPermissions,
+    handleSelectAllPermissions,
+    newRole,
+    updatedPermissions,
+    handleAddRole,
+    setShowAddPermissionModal,
+    setNewRole
+}) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    {entities.map(entity => (
+                        <React.Fragment key={entity}>
+                            <th colSpan={getEntityPermissions(entity).length}>{entity}</th>
+                        </React.Fragment>
+                    ))}
+                    <th colSpan="2">
+                        <button onClick={() => setShowAddPermissionModal(true)}>Añadir Nuevo Permiso</button>
+                    </th>
+                </tr>
+                <tr>
+                    <th>Roles</th>
+                    {entities.flatMap(entity => (
+                        getEntityPermissions(entity).map(permission => (
+                            <React.Fragment key={`${entity}_${permission}`}>
+                                <th key={`${entity}_${permission}`}>{permission}</th>
+                            </React.Fragment>
+                        ))
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {updatedRoles.map(role => (
+                    <React.Fragment key={role.id}>
+                        <tr onMouseOver={() => handleRoleMouseOver(role)} onMouseOut={handleRoleMouseOut}>
+                            <td>
+                                {/* Checkbox para seleccionar/deseleccionar todos los permisos para el rol */}
+                                {selectedRole === role && (
+                                    <input
+                                        type="checkbox"
+                                        checked={selectAllPermissions}
+                                        onChange={handleSelectAllPermissions}
+                                    />
+                                )}
+                                {role.name}
+                            </td>
+                            {entities.flatMap(entity => (
+                                getEntityPermissions(entity).map(permission => (
+                                    <React.Fragment key={`${entity}_${permission}`}>
+                                        <td>
+                                            {role.permissions.includes(`${entity}:${permission}`) ? 'X' : ''}
+                                        </td>
+                                    </React.Fragment>
+                                ))
+                            ))}
+                        </tr>
+                        {roles.some(existingRole => existingRole.name === newRole) && (
+                            <tr>
+                                <td></td>
+                                {entities.flatMap(entity => (
+                                    getEntityPermissions(entity).map(permission => (
+                                        <React.Fragment key={`${entity}_${permission}`}>
+                                            <td>
+                                                {updatedPermissions.includes(`${entity}:${permission}`) ? 'X' : ''}
+                                            </td>
+                                        </React.Fragment>
+                                    ))
+                                ))}
+                                <td colSpan="2"></td>
+                            </tr>
+                        )}
+
+                    </React.Fragment>
+                ))}
+            </tbody>
+
+            <tfoot>
+                <tr>
+                    <td>
+                        {/* Campo para ingresar el nuevo rol */}
+                        <input
+                            type="text"
+                            value={newRole}
+                            onChange={(e) => setNewRole(e.target.value)}
+                            placeholder="Nuevo Rol"
+                        />
+                    </td>
+                    <td colSpan={entities.length * 2}>
+                        <button onClick={handleAddRole}>Añadir Nuevo Rol</button>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+    );
+};
+
+export default RolesTable;
