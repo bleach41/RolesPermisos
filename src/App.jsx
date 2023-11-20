14259dcf3af9ae4a22ef785e2899258cd121c45f
Tony Ale <78830055+bleach41@users.noreply.github.com>
2023-11-20 12:45:50 -0500

    ..esperendo al stilo del modal y los botones

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,85 @@ import RolesPermissionsTable from "./components/RolesPermissionsTable";
 function App() {
 
 
+  // //API
+  // //POST /api/roles:
+  // const createRoles = async (roles) => {
+  //   try {
+  //     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/roles`, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(roles),
+  //     });
+
+  //     const data = await response.json();
+  //     console.log('Roles creados:', data);
+  //   } catch (error) {
+  //     console.error('Error al crear roles:', error);
+  //   }
+  // };
+
+  // // Uso
+  // const rolesToCreate = [
+  //   { name: 'Nuevo Rol', permissions: ['READ', 'WRITE'] },
+  //   // Otros roles que quieras crear
+  // ];
+
+  // createRoles(rolesToCreate);
+
+
+  // //PUT /api/roles:
+  // const updateRoles = async (roles) => {
+  //   try {
+  //     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/roles`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(roles),
+  //     });
+
+  //     const data = await response.json();
+  //     console.log('Roles actualizados:', data);
+  //   } catch (error) {
+  //     console.error('Error al actualizar roles:', error);
+  //   }
+  // };
+
+  // // Uso
+  // const rolesToUpdate = [
+  //   { id: '1', name: 'Rol Actualizado', permissions: ['READ'] },
+  //   // Otros roles que quieras actualizar
+  // ];
+
+  // updateRoles(rolesToUpdate);
+
+  // //DELETE /api/roles:
+  // const deleteRoles = async (roleIds) => {
+  //   try {
+  //     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/roles`, {
+  //       method: 'DELETE',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(roleIds),
+  //     });
+
+  //     const data = await response.json();
+  //     console.log('Roles eliminados:', data);
+  //   } catch (error) {
+  //     console.error('Error al eliminar roles:', error);
+  //   }
+  // };
+
+  // // Uso
+  // const roleIdsToDelete = ['1', '2', '3']; // IDs de los roles A eliminar
+
+  // deleteRoles(roleIdsToDelete);
+
+
+
 
   const rolesArray = [
     { id: "1", name: "Admin", permissions: ["PROJECT:WRITE", "PROJECT:READ", "STORE:READ", "ACCOUNT:READ"] },
