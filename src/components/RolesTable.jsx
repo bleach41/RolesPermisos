736448d68bf109086f6315f5e01f098a9a0cf0e7
Tony Ale <78830055+bleach41@users.noreply.github.com>
2023-11-20 09:32:03 -0500

    SCROLL y algo de estilo

--- src/components/RolesTable.jsx
+++ src/components/RolesTable.jsx
@@ -118,149 +118,154 @@ const RolesTable = ({
 
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                    {entities.map(entity => (
-                        <React.Fragment key={entity}>
-                            <th colSpan={getEntityPermissions(entity).length}
-                                onMouseOver={() => handleEntityMouseOver(entity)} onMouseOut={handleEntityMouseOut}>
-                                {/* Checkbox para seleccionar/deseleccionar todos los permisos para la entidad */}
-                                {selectedEntity === entity && (
-                                    <input
-                                        type="checkbox"
-                                        checked={selectAllEntityPermissions[entity]}
-                                        onChange={handleSelectAllEntityPermissions}
-                                    />
-                                )}
-                                {entity}
-                                {selectedEntity === entity && (
-                                    <img
-                                        className="icon-trash"
-                                        src="/MdiTrashCanOutline_blanco.svg"
-                                        alt="Eliminar"
-                                        onClick={() => handleDeleteEntity(entity)}
-                                    // onMouseOver={() => handleRoleIconMouseOver(role)}
-                                    // onMouseOut={handleRoleIconMouseOut}
-                                    />
-                                )}
-
-                            </th>
-                        </React.Fragment>
-                    ))}
-
-                    <th colSpan="2">
-                        <button onClick={() => setShowAddPermissionModal(true)}>Añadir Nuevo Permiso</button>
-                    </th>
-                </tr>
-                <tr>
-                    <th>Roles</th>
-                    {entities.flatMap(entity => (
-                        getEntityPermissions(entity).map(permission => (
-                            <React.Fragment key={`${entity}_${permission}`}>
-                                <th key={`${entity}_${permission}`}
-                                    onMouseOver={() => handlePermissionMouseOver(`${entity}:${permission}`)}
-                                    onMouseOut={handlePermissionMouseOut}>
-                                    {/* Checkbox para seleccionar/deseleccionar todos los roles para el permiso */}
-                                    {selectedPermission === `${entity}:${permission}` && (
+        <div className="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        {entities.map(entity => (
+                            <React.Fragment key={entity}>
+                                <th colSpan={getEntityPermissions(entity).length}
+                                    onMouseOver={() => handleEntityMouseOver(entity)} onMouseOut={handleEntityMouseOut}>
+                                    {/* Checkbox para seleccionar/deseleccionar todos los permisos para la entidad */}
+                                    {selectedEntity === entity && (
                                         <input
                                             type="checkbox"
-                                            checked={selectAllPermissionRoles[`${entity}:${permission}`]}
-                                            onChange={handleSelectAllPermissionRoles}
+                                            checked={selectAllEntityPermissions[entity]}
+                                            onChange={handleSelectAllEntityPermissions}
                                         />
                                     )}
-                                    {permission}
-                                    {/* Añadir el ícono de eliminación */}
-                                    {selectedPermission === `${entity}:${permission}` && (
+                                    {entity}
+                                    {selectedEntity === entity && (
                                         <img
                                             className="icon-trash"
                                             src="/MdiTrashCanOutline_blanco.svg"
-                                            alt="Eliminar Permiso"
-                                            onClick={() => handlePermissionDelete(`${entity}:${permission}`)}
+                                            alt="Eliminar"
+                                            onClick={() => handleDeleteEntity(entity)}
+                                        // onMouseOver={() => handleRoleIconMouseOver(role)}
+                                        // onMouseOut={handleRoleIconMouseOut}
                                         />
                                     )}
 
-
                                 </th>
                             </React.Fragment>
-                        ))
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {updatedRoles.map(role => (
-                    <React.Fragment key={role.id}>
-                        <tr onMouseOver={() => handleRoleMouseOver(role)} onMouseOut={handleRoleMouseOut}>
-                            <td>
-                                {/* Checkbox para seleccionar/deseleccionar todos los permisos para el rol */}
-                                {selectedRole === role && (
-                                    <input
-                                        type="checkbox"
-                                        checked={selectAllPermissions}
-                                        onChange={handleSelectAllPermissions}
-                                    />
-                                )}
-                                {role.name}
-                                {/* Añadir el ícono de eliminación */}
-                                {selectedRole === role && (
-                                    <img
-                                        className="icon-trash"
-                                        src="/MdiTrashCanOutline.svg"
-                                        alt="Eliminar"
-                                        onClick={() => handleDeleteRole(role.id)}
-                                    // onMouseOver={() => handleRoleIconMouseOver(role)}
-                                    // onMouseOut={handleRoleIconMouseOut}
-                                    />
-                                )}
-                            </td>
-                            {entities.flatMap(entity => (
-                                getEntityPermissions(entity).map(permission => (
-                                    <React.Fragment key={`${entity}_${permission}`}>
-                                        <td>
-                                            {role.permissions.includes(`${entity}:${permission}`) ? 'X' : ''}
-                                        </td>
-                                    </React.Fragment>
-                                ))
-                            ))}
-                        </tr>
-                        {roles.some(existingRole => existingRole.name === newRole) && (
-                            <tr>
-                                <td></td>
+                        ))}
+
+                        <th colSpan="2">
+                            <button onClick={() => setShowAddPermissionModal(true)}>Añadir Nuevo Permiso</button>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>Roles</th>
+                        {entities.flatMap(entity => (
+                            getEntityPermissions(entity).map(permission => (
+                                <React.Fragment key={`${entity}_${permission}`}>
+                                    <th key={`${entity}_${permission}`}
+                                        onMouseOver={() => handlePermissionMouseOver(`${entity}:${permission}`)}
+                                        onMouseOut={handlePermissionMouseOut}>
+                                        {/* Checkbox para seleccionar/deseleccionar todos los roles para el permiso */}
+                                        {selectedPermission === `${entity}:${permission}` && (
+                                            <input
+                                                type="checkbox"
+                                                checked={selectAllPermissionRoles[`${entity}:${permission}`]}
+                                                onChange={handleSelectAllPermissionRoles}
+                                            />
+                                        )}
+                                        {permission}
+                                        {/* Añadir el ícono de eliminación */}
+                                        {selectedPermission === `${entity}:${permission}` && (
+                                            <img
+                                                className="icon-trash"
+                                                src="/MdiTrashCanOutline_blanco.svg"
+                                                alt="Eliminar Permiso"
+                                                onClick={() => handlePermissionDelete(`${entity}:${permission}`)}
+                                            />
+                                        )}
+
+
+                                    </th>
+                                </React.Fragment>
+                            ))
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {updatedRoles.map(role => (
+                        <React.Fragment key={role.id}>
+                            <tr onMouseOver={() => handleRoleMouseOver(role)} onMouseOut={handleRoleMouseOut}>
+                                <td>
+                                    {/* Checkbox para seleccionar/deseleccionar todos los permisos para el rol */}
+                                    {selectedRole === role && (
+                                        <input
+                                            type="checkbox"
+                                            checked={selectAllPermissions}
+                                            onChange={handleSelectAllPermissions}
+                                        />
+                                    )}
+                                    {role.name}
+                                    {/* Añadir el ícono de eliminación */}
+                                    {selectedRole === role && (
+                                        <img
+                                            className="icon-trash"
+                                            src="/MdiTrashCanOutline.svg"
+                                            alt="Eliminar"
+                                            onClick={() => handleDeleteRole(role.id)}
+                                        // onMouseOver={() => handleRoleIconMouseOver(role)}
+                                        // onMouseOut={handleRoleIconMouseOut}
+                                        />
+                                    )}
+                                </td>
                                 {entities.flatMap(entity => (
                                     getEntityPermissions(entity).map(permission => (
                                         <React.Fragment key={`${entity}_${permission}`}>
                                             <td>
-                                                {updatedPermissions.includes(`${entity}:${permission}`) ? 'X' : ''}
+                                                {role.permissions.includes(`${entity}:${permission}`) ? 'X' : ''}
                                             </td>
                                         </React.Fragment>
                                     ))
                                 ))}
-                                <td colSpan="2"></td>
                             </tr>
-                        )}
+                            {roles.some(existingRole => existingRole.name === newRole) && (
+                                <tr>
+                                    <td></td>
+                                    {entities.flatMap(entity => (
+                                        getEntityPermissions(entity).map(permission => (
+                                            <React.Fragment key={`${entity}_${permission}`}>
+                                                <td>
+                                                    {updatedPermissions.includes(`${entity}:${permission}`) ? 'X' : ''}
+                                                </td>
+                                            </React.Fragment>
+                                        ))
+                                    ))}
+                                    <td colSpan="2"></td>
+                                </tr>
+                            )}
 
-                    </React.Fragment>
-                ))}
-            </tbody>
+                        </React.Fragment>
+                    ))}
+                </tbody>
 
-            <tfoot>
-                <tr>
-                    <td>
-                        {/* Campo para ingresar el nuevo rol */}
-                        <input
-                            type="text"
-                            value={newRole}
-                            onChange={(e) => setNewRole(e.target.value)}
-                            placeholder="Nuevo Rol"
-                        />
-                    </td>
-                    <td colSpan={entities.length * 2}>
-                        <button onClick={handleAddRole}>Añadir Nuevo Rol</button>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
+                <tfoot>
+                    <tr>
+                        <td>
+                            {/* Campo para ingresar el nuevo rol */}
+                            <div className="flex-input">
+                                <input
+                                    type="text"
+                                    value={newRole}
+                                    onChange={(e) => setNewRole(e.target.value)}
+                                    placeholder="Nuevo Rol"
+                                />
+                                <img className="agregar-rol" src="/TeenyiconsUpSolid.svg" alt="agregar-Rol" onClick={handleAddRole} />
+                            </div>
+                        </td>
+                        <td colSpan={entities.length * 2}>
+
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
     );
 };
 
